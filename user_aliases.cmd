1bbad22aaaacfb199a74eb06dc2f8aadf2170f49
fconsigny-github <68645708+fconsigny-github@users.noreply.github.com>
2020-08-28 10:42:21 +0200

    Add files via upload

---
+++ user_aliases.cmd
@@ -0,0 +1,27 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat -n "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+sublime= "C:\Users\consigny\softwares\Sublime Text Build 3126 x64\sublime_text.exe" $*
+edit=start nodepad++ $*
+m_build=mvn clean install
+m_test=mvn test
+app_start=mvn spring-boot:run -Dspring-boot.run.profiles=Development,bdd-dev8
+opendir= start .
+draw= "D:\fconsigny\software\cmder\apps\drawio\drawio-portable\drawio-portable.exe"
+paint= mspaint
+clear-browser-cache="D:\fconsigny\software\cmder\apps\scripts\clear-browser-cache\clear-browser-cache.bat"
+ll= ls --show-control-chars -F --color -la $*
+bash_run= cat $* | bash
+
